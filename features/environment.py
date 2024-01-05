23b29e1b19803c218e4b5515f7280478aeb39b41
nazlibutros <bilgen.nazli@gmail.com>
2024-01-05 01:11:18 -0600

    CAP-418

--- features/environment.py
+++ features/environment.py
@@ -16,6 +16,23 @@ def browser_init(context,scenario_name):
     # service = Service(driver_path)
     # context.driver = webdriver.Chrome(service=service)
 
+    # ### MOBILE ####
+    mobile_emulation = {"deviceName": "iPhone 14 Pro Max"}
+    chrome_options = webdriver.ChromeOptions()
+    chrome_options.add_experimental_option("mobileEmulation", mobile_emulation)
+    context.driver = webdriver.Chrome(options=chrome_options)
+    context.driver.implicitly_wait(4)
+    context.driver.wait = WebDriverWait(context.driver, timeout=20)
+    context.app = Application(context.driver)
+
+    # service = Service(
+    #     executable_path='C:\Users\rtbut\OneDrive\Desktop\internship\chromedriver.exe')
+    #
+    # context.driver = webdriver.Chrome(
+    #     options=options,
+    #     service=service
+    # )
+
     # ### FIREFOX ####
     # service = Service(executable_path='/Users/rtbut/OneDrive/Desktop/internship/geckodriver.exe')
     # context.driver = webdriver.Firefox(service=service)
@@ -33,24 +50,24 @@ def browser_init(context,scenario_name):
 
     ### BROWSERSTACK ###
     # Register for BrowserStack, then grab it from https://www.browserstack.com/accounts/settings
-    bs_user = 'ali_59vutx'
-    bs_key = '6qH4Am5Hk9nsG7qU9PCv'
-    url = f'https://{bs_user}:{bs_key}@hub-cloud.browserstack.com/wd/hub'
-    # kevincribman_n1FUEc
-    # fyKSJSXt7QN13oznAzWz
-    options = Options()
-    bstack_options = {
-        'os': 'Windows',
-        'osVersion': '10',
-        'browserName': 'Chrome',
-        'sessionName': scenario_name
-        # 'os': 'OS X',
-        # 'osVersion': 'Big Sur',  # Adjust to the macOS version you are using
-        # 'browserName': 'Chrome',
-        # 'sessionName': scenario_name
-    }
-    options.set_capability('bstack:options', bstack_options)
-    context.driver = webdriver.Remote(command_executor=url, options=options)
+    # bs_user = 'ali_59vutx'
+    # bs_key = '6qH4Am5Hk9nsG7qU9PCv'
+    # url = f'https://{bs_user}:{bs_key}@hub-cloud.browserstack.com/wd/hub'
+    # # kevincribman_n1FUEc
+    # # fyKSJSXt7QN13oznAzWz
+    # options = Options()
+    # bstack_options = {
+    #     'os': 'Windows',
+    #     'osVersion': '10',
+    #     'browserName': 'Chrome',
+    #     'sessionName': scenario_name
+    #     # 'os': 'OS X',
+    #     # 'osVersion': 'Big Sur',  # Adjust to the macOS version you are using
+    #     # 'browserName': 'Chrome',
+    #     # 'sessionName': scenario_name
+    # }
+    # options.set_capability('bstack:options', bstack_options)
+    # context.driver = webdriver.Remote(command_executor=url, options=options)
 
     context.driver.maximize_window()
     ## use it with reely firefox test, maximize window not working ##
